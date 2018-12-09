db7c96f1e22b95b5eeef3616c7d02c23cbe44a1f
mcdhrts <37240726+mcdhrts@users.noreply.github.com>
2018-12-09 23:39:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -426,7 +426,7 @@ function getActiveDevices(hosts) {
 
 function validateIPaddress(inputText) {
     var ipformat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-    if(inputText.value.match(ipformat)) {
+    if(inputText.match(ipformat)) {
         return true;
     } else {
         return false;
